7c1fab3ed6366149d4a8afa043c8ce6cd97f0138
gohb <1174616074@qq.com>
2022-02-16 00:27:35 +0800

    lan3 exercise9-10

--- kern/trap.c
+++ kern/trap.c
@@ -256,7 +256,9 @@ page_fault_handler(struct Trapframe *tf)
 	// Handle kernel-mode page faults.
 
 	// LAB 3: Your code here.
-
+	if(tf->tf_cs && 0x01 == 0) {
+		panic("page_fault in kernel mode, fault address %d\n", fault_va);
+	}
 	// We've already handled kernel-mode exceptions, so if we get here,
 	// the page fault happened in user mode.
 
